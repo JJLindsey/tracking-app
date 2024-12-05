2d53486e464e30e4ecf8bc2c7ea37a9245706a0b
j_lindsey <jlindsey010@outlook.com>
2024-12-05 12:07:38 -0800

    add card, steps and text

--- components/OrderTracking.js
+++ components/OrderTracking.js
@@ -1,7 +1,9 @@
+import { CheckCircle, Inventory, LocalShipping, Schedule } from "@mui/icons-material"
+import { Alert, Box, Card, CardContent, Step, StepLabel, Stepper, Typography } from "@mui/material"
 import React, { useEffect, useState } from "react"
 
 const OrderTracking = () => {
-    const [oderStatus, setOrderStatus] = useState('preparing')
+    const [orderStatus, setOrderStatus] = useState('preparing')
     const [lastestUpdate, setLatestUpdate] = useState('')
     const [webSocketStatus, setWebSocketStatus] = useState('connecting')
     const [socket, setSocket] = useState(null)
@@ -32,4 +34,63 @@ const OrderTracking = () => {
         }
     }, [])
 
+    const statusIcons = (status) => {
+        switch (status) {
+            case 'preparing':
+                return <Inventory />
+            case 'shipping':
+                return <LocalShipping />
+            case 'delivered':
+                return <CheckCircle />
+            default: 
+                return <Schedule />
+        }
+    }
+
+    const steps = ['Order Received', 'Preparing', 'Shipping', 'Delivered']
+    const currentStep = steps.indexOf(orderStatus.charAt(0).toUpperCase() + orderStatus.slice(1))
+
+    return(
+        <Card >
+            <CardContent>
+                <Box>
+                    <Typography>Oder Tracking</Typography>
+                    <Typography>Order #ORDER111</Typography>
+                </Box>
+                {webSocketStatus !== 'connected' && (
+                    <Alert severity="error">Connection Lost. Attempting to reconnect...</Alert>
+                )}
+                <Box
+                    sx={{ 
+                        p: 2, 
+                        mb: 3, 
+                        borderRadius: 1, 
+                        bgcolor: 'background.default',
+                        border: 1,
+                        borderColor: 'divider'
+                    }}
+                >
+                    <Box>
+                        {statusIcons(orderStatus)}
+                        <Box>
+                            <Typography>{orderStatus}</Typography>
+                            {lastestUpdate && (
+                                <Typography>Last updated: {lastestUpdate}
+                                </Typography>
+                            )}
+                        </Box>
+                    </Box>
+                </Box>
+
+                <Stepper activeStep={currentStep}>
+                    {steps.map((label) => (
+                        <Step key={label}>
+                            <StepLabel>{label}</StepLabel>
+                        </Step>
+                    ))}
+                </Stepper>
+            </CardContent>
+        </Card>
+    )
 }
+export default OrderTracking
